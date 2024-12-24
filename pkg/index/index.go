6abc7da571e13f1f834fc37d305ccd49c3ef1c6f
Nagum0 <zackbene9@gmail.com>
2024-12-24 17:28:49 +0100

    Updating the INDEX file works now. However the code is not neccessarily clean and the INDEX file is written as a string.

--- pkg/index/index.go
+++ pkg/index/index.go
@@ -1,39 +1,99 @@
 package index
 
 import (
+	"adda/pkg/errors"
 	"adda/pkg/objects"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
-// A file in the INDEX.
+// An entry in the INDEX.
 type Entry struct {
-    FileName string
+    FilePath string
     Hash     string
     FileType objects.FileType
 }
 
 func (e Entry) String() string {
-    return fmt.Sprintf("%v  %v\t%v", e.FileType, e.Hash, e.FileName)
+    return fmt.Sprintf("%v  %v\t%v", e.FileType, e.Hash, e.FilePath)
 }
 
 // The INDEX file.
+// INDEX file format: filetype hash filepath
 type Index struct {
     // Maps the file paths to the index entries.
     Entries map[string]Entry
 }
 
+func (i Index) String() string {
+    indexString := ""
+    
+    for _, val := range i.Entries {
+        indexString += val.String() + "\n"
+    }
+
+    return indexString
+}
+
 // Parses the INDEX file and retuns a pointer to the Index object.
 func ParseIndex() (*Index, error) {
-    // indexFile, err := os.ReadFile(".adda/INDEX")
-    // if err != nil {
-    //     return nil, nil
-    // }
+    indexFile, err := os.ReadFile(".adda/INDEX")
+    if err != nil {
+        return nil, errors.NewIndexError("Error while reading the INDEX file.")
+    }
+       
+    indexFileString := string(indexFile)
+    
+    if len(indexFileString) == 0 {
+        return &Index{ make(map[string]Entry) }, nil
+    }
 
-    return nil, nil
+    indexFileLines := strings.Split(indexFileString, "\n")
+    indexFileLines = indexFileLines[:len(indexFileLines ) - 1]
+    fmt.Println(" -- TESTING: ", indexFileLines)
+    indexObject := Index{ make(map[string]Entry) }
+    
+    for _, entry := range indexFileLines {
+        splitEntry := strings.Fields(entry)
+
+        if len(splitEntry) != 3 {
+            return nil, errors.NewIndexError(fmt.Sprintf("Corrupted INDEX file: %v", splitEntry))
+        }
+        
+        filePath := splitEntry[2]
+        fileType, _ := strconv.Atoi(splitEntry[0])
+        entryObject := Entry {
+            FilePath: filePath,
+            Hash: splitEntry[1],
+            FileType: objects.FileType(fileType),
+        }
+
+        indexObject.Entries[filePath] = entryObject
+    }
+
+    return &indexObject, nil
 }
 
 // Updates the INDEX file. If the file path already exists in the INDEX 
 // it will only change the hash of the file.
 func (index *Index) Update(blob objects.Blob) error {
+    // When calling this function the hash should already be generated
+    hash, _ := blob.Hash()
+    
+    index.Entries[blob.FilePath] = Entry {
+        FilePath: blob.FilePath,
+        Hash: hash,
+        FileType: blob.Type,
+    }
+    
+    indexFile, err := os.Create(".adda/INDEX")
+    if err != nil {
+        return errors.NewIndexError("Error while updating the INDEX.")
+    }
+    defer indexFile.Close()
+    indexFile.WriteString(index.String())
+
     return nil
 }
