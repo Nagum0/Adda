275b1e61e1c69df1fd8151f7cd5350aafb605753
Nagum0 <zackbene9@gmail.com>
2024-12-25 21:17:03 +0100

    Refactored the integration test for the add command.

--- tests/integration/add_test.go
+++ tests/integration/add_test.go
@@ -10,41 +10,56 @@ import (
 )
 
 func TestAddNewFile(t *testing.T) {
+    files := map[string]string{
+        "test1.txt": "Hello, World!",
+        "test2.txt": Lorem1,
+        "test3.txt": Lorem2,
+    }
+    hashes := make(map[string]string)
+
     defer os.RemoveAll(".adda")
-    defer os.Remove("test.txt")
 
     if err := commands.Init(); err != nil {
         t.Error(err.Error())
     }
 
-    file, err := os.Create("test.txt")
-    if err != nil {
-        t.Error(err.Error())
-    }
-    file.WriteString("Hello, World!")
-    file.Close()
-    
-    blob := objects.NewBlob("test.txt", objects.FILE)
-    hash, _ := blob.Hash()
+    for filePath, text := range files {
+        defer os.Remove(filePath)
 
-    if err = commands.Add("test.txt"); err != nil {
-        t.Error(err.Error())
+        file, err := os.Create(filePath)
+        if err != nil {
+            t.Error(err.Error())
+        }
+        file.WriteString(text)
+        file.Close()
+
+        blob := objects.NewBlob(filePath, objects.FILE)
+        hash, _ := blob.Hash()
+        hashes[filePath] = hash
+
+        if err = commands.Add(filePath); err != nil {
+            t.Error(err.Error())
+        }
+
+        // Check if the blob object was created
+        if _, err = os.Stat(".adda/objects/" + hash[:2] + "/" + hash[2:]); err != nil && os.IsNotExist(err) {
+            t.Errorf("Blob file for hash: %v/%v not found", hash[0:2], hash[2:])
+        }
     }
-    
+
     indexFile, err := index.ParseIndex()
     if err != nil {
         t.Error(err.Error())
     }
 
-    // Check if the INDEX file has the entry
-    entry := indexFile.Entries["test.txt"]
-    if entry.String() != fmt.Sprintf("0  %v\ttest.txt", hash) {
-        t.Errorf("Entry for added file not found in INDEX: %v -> %v", hash, entry.Hash)
-    }
-
-    // Check if the blob object was created
-    if _, err = os.Stat(".adda/objects/" + hash[:2] + "/" + hash[2:]); err != nil && os.IsNotExist(err) {
-        t.Errorf("Blob file for hash: %v/%v not found", hash[0:2], hash[2:])
+    // Check if the INDEX file has the entries
+    for filePath, hash := range hashes {
+        entry := indexFile.Entries[filePath]
+        expectedEntry := fmt.Sprintf("0  %v\t%v", hash, filePath)
+        
+        if entry.String() != expectedEntry {
+            t.Errorf("Expected entry: %v. Received entry: %v", expectedEntry, entry.String())
+        }
     }
 }
 
